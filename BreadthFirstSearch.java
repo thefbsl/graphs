24d96626d9257eaf472f08d2797462260f22df5c
thefbsl <96486069+thefbsl@users.noreply.github.com>
2022-05-28 00:00:19 +0600

    Update BreadthFirstSearch.java

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -1,27 +1,28 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class BreadthFirstSearch<Vertex> extends Search<Vertex>{
+public class BreadthFirstSearch<V> extends Search<V>{
 
-    public BreadthFirstSearch(MyGraph<Vertex> graph, Vertex source) {
+    public BreadthFirstSearch(MyGraph<V> graph, V source) {
         super(source);
         bfs(graph, source);
     }
 
-    private void bfs(MyGraph<Vertex> graph, Vertex current) {
+    private void bfs(MyGraph<V> graph, V current) {
         marked.add(current);
-        Queue<Vertex> queue = new LinkedList<>();
+        Queue<V> queue = new LinkedList<>();
         queue.add(current);
         while (!queue.isEmpty()) {
-            Vertex v = queue.poll();
-            for (Vertex vertex : graph.adjacencyList(v)) {
-                if (!marked.contains(vertex)) {
-                    marked.add(vertex);
-                    edgeTo.put(vertex, v);
-                    queue.add(vertex);
+            V v = queue.poll();
+            for (V vertice : graph.adjacencyList(v)) {
+                if (!marked.contains(vertice)) {
+                    marked.add(vertice);
+                    edgeTo.put(vertice, v);
+                    queue.add(vertice);
                 }
             }
         }
     }
 }
+
 
